d8ba5daa49629f01a692af18ef2009a5a26f69a0
gmartinez3 <gmartinez3@werfen.com>
2022-01-17 16:58:45 +0100

    Refactors code organization and methods just to keep it easier to follow.

--- src/main/java/com/werfen/report/service/ExcelGridReportService.java
+++ src/main/java/com/werfen/report/service/ExcelGridReportService.java
@@ -11,147 +11,162 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Logger;
 
 import static java.util.Objects.nonNull;
 
 public class ExcelGridReportService {
 
     private static final int EXCEL_WITDH_VALUE = 256 * 7;
+    Logger log = Logger.getLogger(GridReportService.class.getName());
 
     public void export(String filePath, String sheetName, GridReportConfiguration gridReportConfiguration, GridPageDataSource dataSource) {
         Workbook workbook = new XSSFWorkbook();
 
-        createDataSheet(dataSource, gridReportConfiguration.getGridColumnConfigurations(), workbook);
-        createSummary(gridReportConfiguration, workbook);
+        addDataSheet(workbook, dataSource, gridReportConfiguration.getGridColumnConfigurations());
+        addSummarySheet(workbook, gridReportConfiguration);
 
-        try {
-            FileOutputStream fileOut = null;
-            fileOut = new FileOutputStream(filePath);
+        try (FileOutputStream fileOut = new FileOutputStream(filePath)) {
             workbook.write(fileOut);
-            fileOut.close();
-        } catch (IOException ex){
-
+        } catch (IOException ex) {
+            log.info("Error exporting to Excel: " + ex.getMessage());
         }
     }
 
-    private void createDataSheet(GridPageDataSource dataSource, List<GridColumnConfiguration> gridColumnConfigurations, Workbook workbook) {
+    private void addDataSheet(Workbook workbook, GridPageDataSource dataSource, List<GridColumnConfiguration> gridColumnConfigurations) {
         Sheet sheet = workbook.createSheet("Data");
-        int row = 0;
+        addDataSheetHeader(sheet, workbook, gridColumnConfigurations);
+        addDataSheetBody(sheet, dataSource, gridColumnConfigurations);
+    }
+
+    private void addDataSheetHeader(Sheet sheet, Workbook workbook, List<GridColumnConfiguration> gridColumnConfigurations) {
         Row gridTitleRow = sheet.createRow(0);
-        for(int col=0;col<gridColumnConfigurations.size();col++) {
-            GridColumnConfiguration gridColumnConfiguration= gridColumnConfigurations.get(col);
+        CellStyle headerStyle = getDataSheetHeaderStyle(workbook);
+        for (int col = 0; col < gridColumnConfigurations.size(); col++) {
+            GridColumnConfiguration gridColumnConfiguration = gridColumnConfigurations.get(col);
             Cell cell = gridTitleRow.createCell(col);
             cell.setCellValue(gridColumnConfiguration.getTranslation());
-            CellStyle headerStyle = workbook.createCellStyle();
-            headerStyle.setFillForegroundColor(IndexedColors.BLUE_GREY.getIndex());
-            headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-
-            XSSFFont font = ((XSSFWorkbook) workbook).createFont();
-            font.setFontName("Arial");
-            font.setBold(true);
-            headerStyle.setFont(font);
             cell.setCellStyle(headerStyle);
             sheet.setColumnWidth(col, gridColumnConfiguration.getWidth().getValue() * EXCEL_WITDH_VALUE);
-
         }
+    }
+
+    private CellStyle getDataSheetHeaderStyle(Workbook workbook) {
+        CellStyle headerStyle = workbook.createCellStyle();
+        headerStyle.setFillForegroundColor(IndexedColors.BLUE_GREY.getIndex());
+        headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+        XSSFFont font = ((XSSFWorkbook) workbook).createFont();
+        font.setFontName("Arial");
+        font.setBold(true);
+        headerStyle.setFont(font);
+        return headerStyle;
+    }
+
+    private void addDataSheetBody(Sheet sheet, GridPageDataSource dataSource, List<GridColumnConfiguration> gridColumnConfigurations) {
+        int rowIndex = 1;
         dataSource.moveFirst();
-        row = createDataRow(dataSource.getCurrentPageRows(),sheet,row + 1 , gridColumnConfigurations);
-        while(dataSource.nextPage()){
-            row = createDataRow(dataSource.getCurrentPageRows(),sheet,row, gridColumnConfigurations);
-        }
-    }
-
-    private int createDataRow(List<GridReportRow> currentPageRows, Sheet sheet, int row, List<GridColumnConfiguration> gridColumnConfigurations) {
-        for(int rowIterator = 0 ;rowIterator<currentPageRows.size();rowIterator++) {
-            Row dataRow = sheet.createRow(row);
-            List<GridReportField> values = currentPageRows.get(rowIterator).getValues();
-            for(int col=0;col<gridColumnConfigurations.size();col++) {
-                GridColumnConfiguration gridColumnConfiguration= gridColumnConfigurations.get(col);
-                Cell cell = dataRow.createCell(col);
-                cell.setCellValue(getValueFromName(values,gridColumnConfiguration.getName()));
-                sheet.setColumnWidth(col, gridColumnConfiguration.getWidth().getValue() * EXCEL_WITDH_VALUE);
+        do {
+            List<GridReportRow> pageRows = dataSource.getCurrentPageRows();
+            for (GridReportRow row : pageRows) {
+                addDataSheetBodyRow(sheet, rowIndex, row, gridColumnConfigurations);
+                rowIndex++;
             }
-            row++;
-        }
-        return row;
+        } while (dataSource.nextPage());
     }
 
-    private String getValueFromName(List<GridReportField> values, String name) {
+    private void addDataSheetBodyRow(Sheet sheet, int rowIndex, GridReportRow row, List<GridColumnConfiguration> gridColumnConfigurations) {
+
+        Row dataRow = sheet.createRow(rowIndex);
+        List<GridReportField> values = row.getValues();
+        for (int col = 0; col < gridColumnConfigurations.size(); col++) {
+            GridColumnConfiguration gridColumnConfiguration = gridColumnConfigurations.get(col);
+            Cell cell = dataRow.createCell(col);
+            cell.setCellValue(getGridReportFieldValue(values, gridColumnConfiguration.getName()));
+            sheet.setColumnWidth(col, gridColumnConfiguration.getWidth().getValue() * EXCEL_WITDH_VALUE);
+        }
+    }
+
+    private String getGridReportFieldValue(List<GridReportField> values, String name) {
         Optional<GridReportField> currentReportField = values.stream().filter(reportField -> name.equals(reportField.getName())).findFirst();
         return currentReportField.orElseThrow(() -> new RuntimeException("The field name '" + name + "' in the XLSX is not valid")).getValue();
     }
 
-
-
-    private void createSummary(GridReportConfiguration gridReportConfiguration, Workbook workbook)  {
+    private void addSummarySheet(Workbook workbook, GridReportConfiguration gridReportConfiguration) {
         try {
-            ReportHeaderConfiguration headerConfiguration = gridReportConfiguration.getHeaderConfiguration();
-            ReportFooterConfiguration reportFooterConfiguration = gridReportConfiguration.getFooterConfiguration();
             Sheet sheet = workbook.createSheet("Summary");
-            Row header = sheet.createRow(0);
-            Cell headerCell  = header.createCell(0) ;
-            headerCell.setCellValue(headerConfiguration.getTitle());
-            FileInputStream inputStream = new FileInputStream( headerConfiguration.getLogoPath() );
 
-            int pictureIndex = workbook.addPicture(IOUtils.toByteArray(inputStream), Workbook.PICTURE_TYPE_PNG);
-            inputStream.close();
-
-            CreationHelper helper = workbook.getCreationHelper();
-            Drawing drawing = sheet.createDrawingPatriarch();
-            ClientAnchor anchor = helper.createClientAnchor();
-            anchor.setCol1(2);
-            anchor.setRow1(0);
-            anchor.setCol2(4);
-            anchor.setRow2(1);
-            Picture pict = drawing.createPicture(anchor, pictureIndex);
-            header.createCell(2) ;
-
-            int row = 2;
-
-
-            if (nonNull(headerConfiguration.getField1())) {
-                createHeaderFieldsData(headerConfiguration.getField1(), sheet, row);
-                row ++;
-            }
-            if (nonNull(headerConfiguration.getField2())) {
-                createHeaderFieldsData(headerConfiguration.getField2(), sheet, row);
-                row++;
-            }
-            if (nonNull(headerConfiguration.getField3())) {
-                createHeaderFieldsData(headerConfiguration.getField3(), sheet, row);
-                row ++;
-            }
-            if (nonNull(headerConfiguration.getField4())) {
-                createHeaderFieldsData(headerConfiguration.getField4(), sheet, row);
-                row ++;
-            }
-            row = row + 2;
-
-            if (nonNull(reportFooterConfiguration.getField1())) {
-                createHeaderFieldsData(reportFooterConfiguration.getField1(), sheet, row);
-                row++;
-            }
-            if (nonNull(reportFooterConfiguration.getField2())) {
-                createHeaderFieldsData(reportFooterConfiguration.getField2(), sheet, row);
-                row++;
-            }
-            if (nonNull(reportFooterConfiguration.getField3())) {
-                createHeaderFieldsData(reportFooterConfiguration.getField3(), sheet, row);
-                row++;
-            }
+            int nextRowIndex = addSummaryTitle(sheet, workbook, gridReportConfiguration.getHeaderConfiguration());
+            nextRowIndex = addSummaryHeaderFields(sheet, nextRowIndex, gridReportConfiguration.getHeaderConfiguration());
+            addSummaryFooterFields(sheet, nextRowIndex, gridReportConfiguration.getFooterConfiguration());
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
-    private void createHeaderFieldsData(GridReportField gridReportField, Sheet sheet, int row) {
-        Row subHeaderFiledRow = sheet.createRow(row);
-        Cell headrFieldCell = subHeaderFiledRow.createCell(0);
-        headrFieldCell.setCellValue(gridReportField.getName());
-        headrFieldCell = subHeaderFiledRow.createCell(1);
-        headrFieldCell.setCellValue(gridReportField.getValue());
+    private int addSummaryTitle(Sheet sheet, Workbook workbook, ReportHeaderConfiguration headerConfiguration) throws IOException {
+        Row header = sheet.createRow(0);
+        Cell headerCell = header.createCell(0);
+        headerCell.setCellValue(headerConfiguration.getTitle());
+        FileInputStream inputStream = new FileInputStream(headerConfiguration.getLogoPath());
+
+        int pictureIndex = workbook.addPicture(IOUtils.toByteArray(inputStream), Workbook.PICTURE_TYPE_PNG);
+        inputStream.close();
+
+        CreationHelper helper = workbook.getCreationHelper();
+        Drawing<?> drawing = sheet.createDrawingPatriarch();
+        ClientAnchor anchor = helper.createClientAnchor();
+        anchor.setCol1(2);
+        anchor.setRow1(0);
+        anchor.setCol2(4);
+        anchor.setRow2(1);
+        drawing.createPicture(anchor, pictureIndex);
+        header.createCell(2);
+
+        return 2;
+    }
+
+    private int addSummaryHeaderFields(Sheet sheet, int rowIndex, ReportHeaderConfiguration headerConfiguration) {
+        if (nonNull(headerConfiguration.getField1())) {
+            addSummaryField(sheet, rowIndex, headerConfiguration.getField1());
+            rowIndex++;
+        }
+        if (nonNull(headerConfiguration.getField2())) {
+            addSummaryField(sheet, rowIndex, headerConfiguration.getField2());
+            rowIndex++;
+        }
+        if (nonNull(headerConfiguration.getField3())) {
+            addSummaryField(sheet, rowIndex, headerConfiguration.getField3());
+            rowIndex++;
+        }
+        if (nonNull(headerConfiguration.getField4())) {
+            addSummaryField(sheet, rowIndex, headerConfiguration.getField4());
+            rowIndex++;
+        }
+        return rowIndex + 2;
+    }
+
+    private void addSummaryFooterFields(Sheet sheet, int rowIndex, ReportFooterConfiguration reportFooterConfiguration) {
+        if (nonNull(reportFooterConfiguration.getField1())) {
+            addSummaryField(sheet, rowIndex, reportFooterConfiguration.getField1());
+            rowIndex++;
+        }
+        if (nonNull(reportFooterConfiguration.getField2())) {
+            addSummaryField(sheet, rowIndex, reportFooterConfiguration.getField2());
+            rowIndex++;
+        }
+        if (nonNull(reportFooterConfiguration.getField3())) {
+            addSummaryField(sheet, rowIndex, reportFooterConfiguration.getField3());
+        }
+    }
+
+    private void addSummaryField(Sheet sheet, int rowIndex, GridReportField gridReportField) {
+        Row subHeaderFiledRow = sheet.createRow(rowIndex);
+        Cell headerFieldCell = subHeaderFiledRow.createCell(0);
+        headerFieldCell.setCellValue(gridReportField.getName());
+        headerFieldCell = subHeaderFiledRow.createCell(1);
+        headerFieldCell.setCellValue(gridReportField.getValue());
     }
 
 }
