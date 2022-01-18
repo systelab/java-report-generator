b5b04c80dfab9aab5ce0e694b148939d7a2af08b
gmartinez3 <gmartinez3@werfen.com>
2022-01-18 11:40:56 +0100

    Moves all Jasper Report references from GridReportService to PdfGridReportService.

--- src/main/java/com/werfen/report/service/GridReportService.java
+++ src/main/java/com/werfen/report/service/GridReportService.java
@@ -1,32 +1,20 @@
 package com.werfen.report.service;
 
 import com.werfen.report.model.*;
-import com.werfen.report.service.template.BaseReportTemplateBuilder;
-import com.werfen.report.service.template.GridReportTemplateBuilder;
 import net.sf.jasperreports.engine.JRException;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.logging.Logger;
 
 public class GridReportService {
-
-
-    Logger log = Logger.getLogger(GridReportService.class.getName());
 
     public File build(GridReportConfiguration gridReportConfiguration, GridPageDataSource gridPageDataSource, ReportFormat reportFormat, PageFormat pageFormat) throws JRException {
 
         String filePath = gridReportConfiguration.getOutputFilePath() + reportFormat.getFileExtension();
-        GridReportTemplateBuilder template = new GridReportTemplateBuilder();
-        template.initJasperDesign("gridReport", pageFormat);
-        template.addHeader(gridReportConfiguration.getHeaderConfiguration());
-        template.addFooter(gridReportConfiguration.getFooterConfiguration());
-        template.addGrid(gridReportConfiguration.getGridColumnConfigurations());
+
         switch (reportFormat) {
             case PDF:
                 PdfGridReportService pdfGridReportService = new PdfGridReportService();
-                pdfGridReportService.exportToPdf(filePath, template.getJasperDesign(), this.getProperties(gridReportConfiguration), new GridReportDataSource(gridPageDataSource));
+                pdfGridReportService.exportToPdf(filePath, gridReportConfiguration, new GridReportDataSource(gridPageDataSource), pageFormat);
                 break;
             case EXCEL:
                 ExcelGridReportService excelGridReportService = new ExcelGridReportService();
@@ -39,10 +27,5 @@ public class GridReportService {
         return new File(filePath);
     }
 
-    private Map<String, Object> getProperties(GridReportConfiguration gridReportConfiguration) {
-        Map<String, Object> parameters = new HashMap<>();
-        parameters.put(BaseReportTemplateBuilder.TITLE_LOGO_PARAMETER, gridReportConfiguration.getHeaderConfiguration().getLogoPath());
-        return parameters;
-    }
 
 }
--- src/main/java/com/werfen/report/service/PdfGridReportService.java
+++ src/main/java/com/werfen/report/service/PdfGridReportService.java
@@ -1,13 +1,17 @@
 package com.werfen.report.service;
 
+import com.werfen.report.model.GridReportConfiguration;
+import com.werfen.report.model.PageFormat;
+import com.werfen.report.service.template.BaseReportTemplateBuilder;
+import com.werfen.report.service.template.GridReportTemplateBuilder;
 import net.sf.jasperreports.engine.*;
-import net.sf.jasperreports.engine.design.JasperDesign;
 import net.sf.jasperreports.engine.export.JRPdfExporter;
 import net.sf.jasperreports.export.SimpleExporterInput;
 import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
 import net.sf.jasperreports.export.SimplePdfExporterConfiguration;
 import net.sf.jasperreports.export.SimplePdfReportConfiguration;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Logger;
 
@@ -15,10 +19,16 @@ public class PdfGridReportService {
 
     Logger log = Logger.getLogger(GridReportService.class.getName());
 
-    public void exportToPdf(String filePath, JasperDesign jasperDesign, Map<String, Object> parameters, JRDataSource ds) throws JRException {
+    public void exportToPdf(String filePath, GridReportConfiguration gridReportConfiguration, JRDataSource ds, PageFormat pageFormat) throws JRException {
 
-        JasperReport jasperReport = JasperCompileManager.compileReport(jasperDesign);
-        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, ds);
+        GridReportTemplateBuilder template = new GridReportTemplateBuilder();
+        template.initJasperDesign("gridReport", pageFormat);
+        template.addHeader(gridReportConfiguration.getHeaderConfiguration());
+        template.addFooter(gridReportConfiguration.getFooterConfiguration());
+        template.addGrid(gridReportConfiguration.getGridColumnConfigurations());
+
+        JasperReport jasperReport = JasperCompileManager.compileReport(template.getJasperDesign());
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, this.getProperties(gridReportConfiguration), ds);
 
         JRPdfExporter exporter = new JRPdfExporter();
         exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
@@ -46,5 +56,11 @@ public class PdfGridReportService {
         exportConfig.setEncrypted(true);
         exportConfig.setAllowedPermissionsHint("PRINTING");
         return exportConfig;
+    }
+
+    private Map<String, Object> getProperties(GridReportConfiguration gridReportConfiguration) {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put(BaseReportTemplateBuilder.TITLE_LOGO_PARAMETER, gridReportConfiguration.getHeaderConfiguration().getLogoPath());
+        return parameters;
     }
 }
