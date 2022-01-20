c85e9e19ebe6f5ecbc654b94851f34c30fc42845
alfonsserra <alfonsoserra@gmail.com>
2022-01-20 12:58:40 +0100

    roups the constants.

--- src/main/java/com/werfen/report/service/template/BaseReportTemplateBuilder.java
+++ src/main/java/com/werfen/report/service/template/BaseReportTemplateBuilder.java
@@ -19,13 +19,13 @@ import static java.util.Objects.nonNull;
 public class BaseReportTemplateBuilder {
 
     public static final String BOLD_FONT = "Helvetica-Bold";
-    public static final String TITLE_LOGO_PARAMETER = "TITLE_LOGO_PATH";
+
     protected static final int PAGE_MARGIN = 20;
-    private static final int TITLE_MARGIN_X = 10;
-    private static final int HEADER_MARGIN_X = 10;
-    private static final int FOOTER_MARGIN_X = 10;
-    private static final int TITLE_HEIGHT = 42;
     private static final int PAGE_NUMBER_MARGIN_X = 14;
+
+    public static final String TITLE_LOGO_PARAMETER = "TITLE_LOGO_PATH";
+    private static final int TITLE_MARGIN_X = 10;
+    private static final int TITLE_HEIGHT = 42;
     private static final int TITLE_Y = 12;
     private static final int TITLE_LOGO_WIDTH = 81;
     private static final int TITLE_LOGO_HEIGHT = 26;
@@ -38,18 +38,20 @@ public class BaseReportTemplateBuilder {
     private static final int TITLE_LINE_SEPARATOR_HEIGHT = 42;
     private static final int TITLE_LINE_SEPARATOR_Y = 0;
     private static final Float TITLE_TEXT_FONT_SIZE = 14f;
+    private static final int TITLE_X = TITLE_MARGIN_X;
 
+    private static final int HEADER_MARGIN_X = 10;
     private static final int HEADER_HEIGHT = 48;
     private static final int HEADER_FIELDS_HEIGHT = 11;
     private static final int HEADER_FIELDS_TITLE_Y = 7;
     private static final int HEADER_FIELDS_Y = 21;
-    private static final int TITLE_X = TITLE_MARGIN_X;
     private static final int HEADER_FRAME_HEIGHT = 38;
     private static final int HEADER_FRAME_X = 0;
     private static final int HEADER_FRAME_Y = 0;
-
-    private static final int FOOTER_TEXT_FIELD_HEIGHT = 9;
     private static final int HEADER_FIELDS_INITIAL_X = HEADER_MARGIN_X;
+
+    private static final int FOOTER_MARGIN_X = 10;
+    private static final int FOOTER_TEXT_FIELD_HEIGHT = 9;
     private static final int FOOTER_FIELDS_TITLE_Y = 8;
     private static final int FOOTER_FIELDS_Y = 17;
     private static final int FOOTER_PAGE_NUMBER_TEXT_WIDTH = 32;
@@ -58,11 +60,11 @@ public class BaseReportTemplateBuilder {
     private static final int FOOTER_FRAME_X = 0;
     private static final int FOOTER_FRAME_Y = 0;
     private static final int FOOTER_HEIGHT = 34;
+    private static final int FOOTER_FIELDS_INITIAL_X = FOOTER_MARGIN_X;
 
     private static final String STYLE_NAME = "default";
     private static final String STYLE_ENCODING = "UTF-8";
     private static final Float STYLE_FONT_SIZE = 7f;
-    private static final int FOOTER_FIELDS_INITIAL_X = FOOTER_MARGIN_X;
 
     protected JasperDesign jasperDesign;
 
