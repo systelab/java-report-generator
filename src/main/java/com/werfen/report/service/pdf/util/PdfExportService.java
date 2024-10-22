f9a43f710a85fcd0e7783a674c5864c4e3d7af47
WERFEN\gmartinez3 <gmartinez3@werfen.com>
2024-10-22 11:20:27 +0200

    BEASW-17784: Updates JasperReports library to 7.0.1 and replaces deprecated JRPdfExporter by the simpler JasperExportManager static class.

--- src/main/java/com/werfen/report/service/pdf/util/PdfExportService.java
+++ src/main/java/com/werfen/report/service/pdf/util/PdfExportService.java
@@ -1,12 +1,8 @@
 package com.werfen.report.service.pdf.util;
 
 import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.export.JRPdfExporter;
-import net.sf.jasperreports.export.SimpleExporterInput;
-import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
-import net.sf.jasperreports.export.SimplePdfExporterConfiguration;
-import net.sf.jasperreports.export.SimplePdfReportConfiguration;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -14,40 +10,13 @@ import java.io.File;
 public class PdfExportService {
 
     public File exportToFile(JasperPrint jasperPrint, String filePath) throws JRException {
-        JRPdfExporter exporter = exportDocument(jasperPrint, new SimpleOutputStreamExporterOutput(filePath));
-
-        exporter.exportReport();
+        JasperExportManager.exportReportToPdfFile(jasperPrint, filePath);
         return new File(filePath);
     }
 
     public ByteArrayOutputStream exportToStream(JasperPrint jasperPrint) throws JRException {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        SimpleOutputStreamExporterOutput simpleOutputStreamExporterOutput = new SimpleOutputStreamExporterOutput(outputStream);
-        JRPdfExporter exporter = exportDocument(jasperPrint, simpleOutputStreamExporterOutput);
-
-        exporter.exportReport();
+        JasperExportManager.exportReportToPdfStream(jasperPrint, outputStream);
         return outputStream;
-    }
-
-    private JRPdfExporter exportDocument(JasperPrint jasperPrint, SimpleOutputStreamExporterOutput simpleOutputStreamExporterOutput) {
-        JRPdfExporter exporter = new JRPdfExporter();
-        exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
-        exporter.setExporterOutput(simpleOutputStreamExporterOutput);
-        exporter.setConfiguration(getPdfReportConfiguration());
-        exporter.setConfiguration(getPdfExporterConfiguration());
-        return exporter;
-    }
-
-    private SimplePdfReportConfiguration getPdfReportConfiguration() {
-        SimplePdfReportConfiguration reportConfig = new SimplePdfReportConfiguration();
-        reportConfig.setSizePageToContent(true);
-        reportConfig.setForceLineBreakPolicy(false);
-        return reportConfig;
-    }
-
-    private SimplePdfExporterConfiguration getPdfExporterConfiguration() {
-        SimplePdfExporterConfiguration exportConfig = new SimplePdfExporterConfiguration();
-        exportConfig.setAllowedPermissionsHint("PRINTING");
-        return exportConfig;
     }
 }
